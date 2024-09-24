a706cd7b8fc17c004c737be766cbf202c01f0c40
dean <dean@beamdot3d.com>
2024-09-24 14:46:17 -0600

    new log

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 import jsPDF from "jspdf";
 console.log("page 5");
-console.log("Sept 24, 2024 - Stack Builder");
+console.log("Sept 24, 2024 - Stack Builder - Test 1");
 
 // const myNum = 55;
 // console.log("myNum: ", toString(myNum));
