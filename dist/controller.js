736da984fc5360ee28f1ac33d5c1ae13a5eeafd8
dean <dean@beamdot3d.com>
2024-10-19 10:51:40 -0600

    cross notes started

--- dist/controller.js
+++ dist/controller.js
@@ -28286,7 +28286,7 @@
   var pdfView_default = new PDFView();
 
   // src/controller.js
-  console.log("Descale - Oct 19, 2024");
+  console.log("Cross-Notes - Oct 19, 2024");
   var controlStackBtns = function(arrayEl) {
     stackView_default._retarget();
     const compVal = arrayEl.attributes.class.nodeValue.split(" ")[1];
